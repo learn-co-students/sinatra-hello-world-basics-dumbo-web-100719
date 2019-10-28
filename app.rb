08ea0abf27fff1b8355344e2b64ad81286ac5c55
Muna Mashhour <munamashhour@gmail.com>
2019-10-28 17:19:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
     "Hello, World!"
   end 
 
+  get '/name' do
+    "My name is Muna"
+  end 
+
+  get '/hometown' do
+    "My hometown is Ramallah"
+  end 
+
+  get '/favorite-song' do
+    "My favorite song is idk"
+  end 
 
 end
